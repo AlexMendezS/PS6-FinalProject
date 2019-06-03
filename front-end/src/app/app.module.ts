373e32d25d30a6f1810863a0bc06a518a448a57b
nassou78 <jogafni@gmail.com>
2019-06-03 10:29:32 +0200

    Ajout du component waitlist

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {WaitlistComponent} from './waitlist/waitlist.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const appRoutes: Routes = [
+  {path: '', component: WaitlistComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WaitlistComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
