6cbdbd68617a4f3c7d852a0f0017afc532d0c7e0
nassou78 <jogafni@gmail.com>
2019-06-03 10:50:06 +0200

    Ajout du model user

---
+++ front-end/src/models/User.model.ts
@@ -0,0 +1,21 @@
+export class User {
+  constructor(
+
+    public studentNumber?: string,
+    public name?: string,
+    public firstname?: string,
+    public status?: string,
+    public notif?: string,
+    public mail?: string,
+    public password?: string,
+    public birthday?: string,
+    public gender?: string,
+    public nationality?: string,
+    public address?: string,
+    public zip?: string,
+    public city?: string,
+    public phone?: string,
+    public educationStream?: string,
+    public id?: number,
+  ) {}
+}
