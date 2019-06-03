1f85a4c104938b94fb424586977b0d361b20b9d8
Nidhal Mahjoub <mahjoubnidhal@gmail.com>
2019-06-03 14:44:30 +0200

    Android Studio

---
+++ PS6-final/app/src/main/java/com/example/ps6/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.ps6;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.JsonArray;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mTextViewResult;
+    private RequestQueue requestQueue;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button buttonParse = findViewById(R.id.button_parse);
+        mTextViewResult = findViewById(R.id.text_view_result);
+        String URL = "http://10.212.118.37:9428/api/students";
+
+        requestQueue = Volley.newRequestQueue(this);
+
+
+        JsonArrayRequest arrayRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                URL,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.e("Rest Response", response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Rest error Response", error.toString());
+
+
+                    }
+                }
+        );
+
+        requestQueue.add(arrayRequest);
+    }
+
+
+}
